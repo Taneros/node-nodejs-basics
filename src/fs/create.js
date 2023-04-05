3ebfd93f72200269cf063b65b05042c83170b872
rfathullin <rfathullin@luxms.com>
2023-04-05 12:33:42 +0300

    feat: add create fresh funcitonality

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs from 'fs'
+import path from 'path'
+
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+const content = 'I am fresh and young'
+
 const create = async () => {
-    // Write your code here 
+    
+    try {
+        fs.writeFileSync(path.join(__dirname, '/files', 'fresh.txt'), content)    
+
+    } catch (error) {
+        console.error(error);
+        
+    }
+    
 };
 
 await create();
