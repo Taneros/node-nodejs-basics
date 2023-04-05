85ae6b0192c38ef293bdd130c57e48d5c960d1c6
rfathullin <rfathullin@luxms.com>
2023-04-05 17:36:41 +0300

    feat: finish rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
-const rename = async () => {
-    // Write your code here 
+import fs from 'fs'
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const scriptPath=fileURLToPath(import.meta.url)
+
+const dirName=path.dirname(scriptPath)
+
+const pathToWrongFile=path.join(dirName,'files', 'wrongFilename.txt')
+
+const pathToCorrectFile=path.join(dirName,'files','properFilename.md')
+
+const rename=async () => {
+    if(fs.existsSync(pathToWrongFile)&&!fs.existsSync(pathToCorrectFile)) {
+        
+        fs.renameSync(pathToWrongFile, pathToCorrectFile)
+        
+    } else {
+        throw new Error ('FS operation failed')
+    }
 };
 
 await rename();
