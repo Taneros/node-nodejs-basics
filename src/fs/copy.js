4cf4fe5b9ea953c8502a641406697d0d8f115006
rfathullin <rfathullin@luxms.com>
2023-04-05 17:15:30 +0300

    feat: finish copy folder files to files_copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,32 @@
+import fs from 'fs'
+
+import path from 'path'
+
+import {fileURLToPath} from 'url'
+
+const fileName = fileURLToPath(import.meta.url)
+
+const dirName = path.dirname(fileName)
+
+const pathToOriginDir = path.join(dirName,'files')
+
+const pathToNewDir =path.join(dirName,'files_copy')
+
+
 const copy = async () => {
-    // Write your code here 
+    if(!fs.existsSync(pathToNewDir) && !fs.existsSync(pathToNewDir) ) {
+            fs.mkdirSync(pathToNewDir)
+
+            fs.readdirSync(pathToOriginDir).forEach((file) => {
+                console.log(file);
+                fs.copyFileSync(path.join(pathToOriginDir, file), path.join(pathToNewDir, file))
+            })
+
+        } else {
+            throw new Error('FS operation failed')
+        }
+
+
 };
 
 await copy();
