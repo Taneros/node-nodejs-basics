c6c211788f480fbd31cd0f396d0e7441ca3ed1c9
rfathullin <rfathullin@luxms.com>
2023-04-05 17:15:13 +0300

    refactor: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -3,11 +3,11 @@ import path from 'path'
 
 import {fileURLToPath} from 'url';
 
-const __filename=fileURLToPath(import.meta.url);
+const fileName=fileURLToPath(import.meta.url);
 
-const __dirname=path.dirname(__filename);
+const dirName=path.dirname(fileName);
 
-const pathToFile=path.join(__dirname,'files','fresh.txt')
+const pathToFile=path.join(dirName,'files','fresh.txt')
 
 const content='I am fresh and young'
 
