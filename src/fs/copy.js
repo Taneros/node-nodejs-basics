0028c0499baa6d21b9de8f8cee689a1d7a62251a
rfathullin <rfathullin@luxms.com>
2023-04-05 19:07:12 +0300

    fix: copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -2,28 +2,35 @@ import fs from 'fs'
 import path from 'path'
 import {fileURLToPath} from 'url'
 
-const scriptPath = fileURLToPath(import.meta.url)
+const scriptPath=fileURLToPath(import.meta.url)
 
-const dirName = path.dirname(scriptPath)
+const dirName=path.dirname(scriptPath)
 
-const pathToOriginDir = path.join(dirName,'files')
+const pathToOriginDir=path.join(dirName,'files')
 
-const pathToNewDir =path.join(dirName,'files_copy')
+const pathToNewDir=path.join(dirName,'files_copy')
 
 
-const copy = async () => {
-    if(!fs.existsSync(pathToNewDir) && !fs.existsSync(pathToNewDir) ) {
-            fs.mkdirSync(pathToNewDir)
+const copy=async () => {
 
-            fs.readdirSync(pathToOriginDir).forEach((file) => {
-                const oldFilePath = path.join(pathToOriginDir, file)
-                const newFilePath = path.join(pathToNewDir, file)
-                fs.copyFileSync(oldFilePath, newFilePath)
-            })
+    try {
+        const filesToCopy=fs.readdirSync(pathToOriginDir)
 
-        } else {
-            throw new Error('FS operation failed')
-        }
+        fs.mkdirSync(pathToNewDir)
+
+        filesToCopy.forEach((file) => {
+            const oldFilePath=path.join(pathToOriginDir,file)
+            const newFilePath=path.join(pathToNewDir,file)
+            fs.copyFileSync(oldFilePath,newFilePath)
+        })
+        
+        console.log('\Copied successfully!')
+
+    } catch(error) {
+        throw new Error('FS operation failed')
+    }
+
+
 
 
 };
